ffa84a7122f06f5128674f789e397669c3b1c555
driele-torres <drieleb.torres@gmail.com>
2015-04-09 20:59:47 +0100

    image

--- app/src/main/java/com/example/kelly/logeasyfinal/LogEasy.java
+++ app/src/main/java/com/example/kelly/logeasyfinal/LogEasy.java
@@ -1,17 +1,45 @@
 package com.example.kelly.logeasyfinal;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class LogEasy extends ActionBarActivity {
 
+
+public class LogEasy extends Activity {
+    Button btnStart;
+    Button btnInfo;
+    Intent intent = new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_easy);
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnInfo  = (Button)findViewById(R.id.btnInfo);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(LogEasy.this,Choose_Player.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogEasy.this,Choose_Player.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
 
