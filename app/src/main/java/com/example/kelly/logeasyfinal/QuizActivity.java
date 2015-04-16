8799ce1119c3cd776208b49a294821860e597100
LainePP <elainepportela@gmail.com>
2015-04-16 18:15:46 +0100

    Lesson Screen

--- app/src/main/java/com/example/kelly/logeasyfinal/QuizActivity.java
+++ app/src/main/java/com/example/kelly/logeasyfinal/QuizActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
